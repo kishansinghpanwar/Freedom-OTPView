6c8bf47b7f51cd001da8f166cfc3ce2780f17418
Kishan Singh <singhkishanpanwar@gmail.com>
2022-07-03 20:52:01 +0530

    Added button for validate OTP & Updated screenshots

--- app/src/main/java/com/app/freedomotpviewexample/MainActivity.kt
+++ app/src/main/java/com/app/freedomotpviewexample/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.app.freedomotpviewexample
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.freedomotpview.OTPView
 
 class MainActivity : AppCompatActivity() {
     var otpView: OTPView? = null
+    var btnGetOtp: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,5 +26,18 @@ class MainActivity : AppCompatActivity() {
             .setBackground(ContextCompat.getDrawable(this, R.drawable.bg_otp_rectangle_code))
             .build()
         otpView?.setConfiguration(configuration)
+
+        btnGetOtp = findViewById(R.id.btnGetOtp)
+        btnGetOtp?.setOnClickListener {
+            if (otpView?.isOTPValid == true) {
+                showToast(getString(R.string.your_otp_is, otpView?.otp, otpView?.otpLength))
+            } else {
+                showToast(getString(R.string.invalid_otp))
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
     }
 }
